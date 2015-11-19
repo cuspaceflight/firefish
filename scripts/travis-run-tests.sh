643f73036bdd842c9372e72d0c5cf39dff9a5732
Rich Wareham <rjw57@cam.ac.uk>
2015-11-19 13:28:15 +0000

    travis: add running of test suite via tox
    Travis allows for multiple different configurations to be tested. Add
    configuration to .travis.yml which specifies a single tox testenv to be run. Add
    command to travis-install.sh which installs tox and a few lines to
    travis-run-tests.sh which run tox if the TOX_ENV environment variable is set.

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+#
 # Run tests on Travis CI worker. Requires that travis-install.sh has been run.
 
-# FIXME: we currently have no tests!
-true
+# Log commands and exit on error
+set -xe
+
+# If the TOX_ENV environment variable is set, run tox for that testenv.
+if [ ! -z "${TOX_ENV}" ]; then
+    tox -e "${TOX_ENV}"
+fi
 
