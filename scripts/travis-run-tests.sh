e9b146ed8a8bcaebf5d530408423cf5baf69704d
Rich Wareham <rjw57@cam.ac.uk>
2015-11-20 16:31:08 +0000

    travis: add OpenFOAM to path when running tests
    Not that it matters yet, but having installed OpenFOAM it would be nice if the
    travis scripts actually add it to the PATH(!).

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -2,8 +2,15 @@
 #
 # Run tests on Travis CI worker. Requires that travis-install.sh has been run.
 
-# Log commands and exit on error
-set -xe
+# Exit on error
+set -e
+
+# We need to add the OpenFOAM applications to the PATH. OpenFOAM comes with a
+# script to do this for us.
+source /opt/openfoam30/etc/bashrc
+
+# Now switch on logging so that we don't get log spam from the OpenFOAM setup
+set -x
 
 # If the TOX_ENV environment variable is set, run tox for that testenv.
 if [ ! -z "${TOX_ENV}" ]; then
