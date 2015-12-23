d4beb352bdfa5538ffce43d2d4780205f0804304
boyuanxiao <bx215@cam.ac.uk>
2015-12-23 11:02:45 +0000

    plot supersonic speeds for Mach 3 and some representative fin paramaters

--- examples/plot_fin_flutter.py
+++ examples/plot_fin_flutter.py
@@ -25,15 +25,17 @@ def main(output='flutter-velocity-example.pdf'):
     """
     zs = np.linspace(0, 50000, 200)
     ps, ts, ss = model_atmosphere(zs)
-    rhos = ps / (0.2869 * (ts + 273.1))
+    rhos = (ps/1000) / (0.2869 * (ts + 273.1))
     vs_t = flutter_velocity_transonic(ps, ss, 20, 10, 10, 0.2)
     vs_s = flutter_velocity_supersonic(rhos, 380, 104, 1, 0.3, 0.0313, 0.0855, 3)
 
     plt.figure()
-    plt.plot(zs * 1e-3, vs_t)
+    plt.plot(zs * 1e-3, vs_t, 'r', label="transonic flutter velocity")
+    plt.plot(zs*1e-3, vs_s, 'g', label="supersonic flutter velocity, Mach 3")
     plt.title('Flutter velocity vs altitude')
     plt.xlabel('Altitude [km]')
     plt.ylabel('Flutter velocity [m/s]')
+    plt.legend()
     plt.savefig(output, format='PDF')
 
 if __name__ == '__main__':
